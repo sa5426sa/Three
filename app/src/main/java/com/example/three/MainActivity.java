1649b35b8fc9b4d3592b9ce5f645c0ee5697c43a
Shaked <sa5426@bs.amalnet.k12.il>
2022-12-05 12:14:30 +0200

    initial commit

---
+++ app/src/main/java/com/example/three/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.three;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int counter=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+    }
+
+    public void clicked(View view) {
+        counter++;
+        if(counter==7)
+        {
+            btn.setText("enough clicks. restart!");
+            counter=0;
+        }
+        else btn.setText("clicks: " + counter);
+    }
+}
